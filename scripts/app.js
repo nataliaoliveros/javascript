fe70a4297a107984396c9118df912ab369e1c992
Natalia Oliveros <natysandy@gmail.com>
2018-09-24 16:13:07 -0500

    pruebas con variables javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,43 @@
-console.log("Hola Mundo");
+/*
+var numero1= 10.5;
+var numero2= 5;
+var resultado= numero1*numero2;
+
+console.log(resultado);
+
+var nombre = "Natalia Oliveros ";
+var profesion = "Desarrollador full stack";
+
+console.log(nombre + profesion) 
+
+var tiene_mascota = true;
+
+console.log(tiene_mascota)
+
+var datos = ["Natalia",26,false,25,78,"Desarrollador"];
+
+console.log(datos[2]);
+
+var edad = 41;
+
+if (edad>=18 && edad<=40){
+	console.log("eres un adulto joven");
+}
+else{
+	console.log("No eres un adulto joven");
+}
+var dias = ["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+
+for(var i=0;i<7;i++){
+	console.log(dias[i]);
+}
+*/
+
+function suma(numero1,numero2){
+var resultado = numero1+numero2;
+return resultado;
+}
+
+var res=suma(88,55);
+
+console.log(res);
