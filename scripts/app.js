5832faa05bb7614710d7d41f5d0d6d4f28461028
Natalia Oliveros <natysandy@gmail.com>
2018-09-25 15:49:33 -0500

    version iva

--- scripts/app.js
+++ scripts/app.js
@@ -32,7 +32,6 @@ for(var i=0;i<7;i++){
 	console.log(dias[i]);
 }
 
-
 function suma(numero1,numero2){
 var resultado = numero1+numero2;
 return resultado;
@@ -41,7 +40,6 @@ return resultado;
 var res=suma(88,55);
 
 console.log(res);
-*/
 
 function iva (precio_producto){
 	var iva_producto = precio_producto* 0.19;
@@ -51,3 +49,33 @@ function iva (precio_producto){
 var resultado = iva(1000);
 
 console.log(resultado);
+
+var titulos = document.getElementsByTagName('h1');
+
+console.log(titulos)
+
+var clasetexto = document.getElementsByClassName("texto");
+
+console.log(clasetexto);
+
+var sede = document.getElementById("sede");
+
+console.log(sede);
+*/
+
+function iva (precio_producto,porcentaje_iva){
+	var iva_producto = precio_producto* (porcentaje_iva/100);
+ 	return iva_producto;
+}
+
+
+function calcular_iva(){
+	var precio = parseInt (document.getElementById('precio').value);
+	var porcentaje_iva = parseInt (document.getElementById('iva').value);
+
+	var resultado = iva(precio, porcentaje_iva);
+	var salida = document.getElementById('resultado');
+	var texto = document.createTextNode(resultado);
+	salida.appendChild(texto);
+	console.log(resultado);
+}
